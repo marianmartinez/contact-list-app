66febd28b52d64f8b4799381d0349f6699660b67
Marian <marian.mtnz@gmail.com>
2015-06-14 23:30:50 +0200

    Basic layout

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,4 +2,10 @@ var contactListApp = angular.module('contactListApp', ['ngMaterial']);
 
 contactListApp.controller('contactListCtrl', function($scope){
 
+  $scope.contacts = [
+    {'name': 'Pepe', 'email': 'pepe@pepe.com', 'phone': '666831799'},
+    {'name': 'Juan', 'email': 'juan@juan.com', 'phone': '630444444'},
+    {'name': 'Ana', 'email': 'ana@ana.com', 'phone': '680555555'}
+  ];
+
 });
